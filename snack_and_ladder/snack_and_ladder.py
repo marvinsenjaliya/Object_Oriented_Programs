98a8fbe76a8ffb77d1ed2532978bdfc8e10757ee
marvinsenjaliya <marvinsenjaliya@gmail.com>
2020-01-31 19:14:59 +0530

    Added:player started with zero position

--- snack_and_ladder/snack_and_ladder.py
+++ snack_and_ladder/snack_and_ladder.py
@@ -3,3 +3,11 @@ Created on :1/31/2020
 @author:Marvin Senjaliya
 """
 print("Welcome to snack and ladder game")
+
+class Game:
+
+    def __init__(self):
+        self.position=0
+
+player=Game()
+player.position
